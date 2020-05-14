c24b7b9ba429f2c62f4d659b7c939aa568aa3b28
Industrylaud <mrfros22@gmail.com>
2020-05-14 02:35:22 +0200

    fix to model

--- Models/CharacterSheet.cs
+++ Models/CharacterSheet.cs
@@ -11,6 +11,7 @@ namespace RPGHelper.Models
     {
         public int Id { get; set; }
 
+        [Required]
         [ForeignKey("ApplicationUser")]
         public int ApplicationUserId { get; set; }
         public ApplicationUser User { get; set; }
