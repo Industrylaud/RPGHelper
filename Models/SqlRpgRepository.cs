f45d921b8df5a850137577bf7a133ed42870f07d
Industrylaud <mrfros22@gmail.com>
2020-05-19 17:23:10 +0200

    add sql rpg repo

---
+++ Models/SqlRpgRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPGHelper.Models
+{
+    public class SqlRpgRepository : ISqlRpgRepository
+    {
+        public CharacterSheet Add(CharacterSheet characterSheet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CharacterSheet Edit(CharacterSheet characterSheet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CharacterSheet Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CharacterSheet> GetAllSheets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CharacterSheet Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
