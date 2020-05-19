062fff7a601da3c5238a6dc8f07ffd5c035a8bd4
Industrylaud <mrfros22@gmail.com>
2020-05-19 17:21:51 +0200

    add ISqlRpgRepo

---
+++ Models/ISqlRpgRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPGHelper.Models
+{
+    interface ISqlRpgRepository
+    {
+        CharacterSheet Add(CharacterSheet characterSheet);
+
+        CharacterSheet Remove(int Id);
+
+        CharacterSheet Get(int Id);
+
+        IEnumerable<CharacterSheet> GetAllSheets();
+
+        CharacterSheet Edit(CharacterSheet characterSheet);
+    }
+}
