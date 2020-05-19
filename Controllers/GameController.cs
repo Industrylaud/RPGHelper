be785e72dce7fd9f5772feccd5efe099baac67a5
Industrylaud <mrfros22@gmail.com>
2020-05-19 17:09:08 +0200

    add constructor

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RPGHelper.Models;
 
 namespace RPGHelper.Controllers
 {
     public class GameController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public GameController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
